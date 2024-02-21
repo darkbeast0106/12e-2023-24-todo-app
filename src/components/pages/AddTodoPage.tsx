2b0450be44600cffad2a4c8c6d3a71b5b34737d8
Budaházi Bence <budahazi.bence@petrik.hu>
2024-02-21 13:55:57 +0100

    feat: #3 felvétel megvalósítása

--- src/components/pages/AddTodoPage.tsx
+++ src/components/pages/AddTodoPage.tsx
@@ -1,9 +1,30 @@
+import { FormEvent, useRef } from "react";
+
 function AddTodoPage() {
+    const url = "https://retoolapi.dev/CVz2rC/todo";
+    const todoRef = useRef<HTMLInputElement>(null);
+
+    async function addNewTodo(event:FormEvent) {
+        event.preventDefault();
+        const todoName = todoRef.current?.value;
+        const todo = {name: todoName, done: false}
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(todo),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        if (response.ok) {
+            // TODO: sikeres felvétel
+        }
+    }
+
     return ( <>
-        <form>
+        <form onSubmit={event => addNewTodo(event)}>
             <div>
                 <label htmlFor="todo">Teendő</label>
-                <input type="text" id="todo" required/>
+                <input type="text" id="todo" required ref={todoRef}/>
             </div>
             <button type="submit">Felvétel</button>
         </form>
