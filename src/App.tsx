e0ee1b056a4cb6c47d9f77f624cfb91ae3011f6c
Budaházi Bence <budahazi.bence@petrik.hu>
2024-02-21 13:42:31 +0100

    feat: oldalak közötti navigáció

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
     <>
+    <nav>
+      <ul>
+        <li><Link to="/">Teendők</Link></li>
+        <li><Link to="/add">Új teendő felvétele</Link></li>
+      </ul>
+    </nav>
     <main>
       <Outlet />
     </main>
